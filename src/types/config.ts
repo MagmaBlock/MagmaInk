d0ea4a5bf8016bb5eca9232b6e92d516f17a5b04
岩浆块Magma <magmablock@qq.com>
2024-06-30 17:22:04 +0800

    格式化

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,61 +1,64 @@
-import type { LIGHT_MODE, DARK_MODE, AUTO_MODE } from "@constants/constants"
+import type { LIGHT_MODE, DARK_MODE, AUTO_MODE } from "@constants/constants";
 
 export type SiteConfig = {
-  title: string
-  subtitle: string
+	title: string;
+	subtitle: string;
 
-  lang: string
+	lang: string;
 
-  themeColor: {
-    hue: number
-    fixed: boolean
-  }
-  banner: {
-    enable: boolean
-    src: string
-    position?: string
-  }
+	themeColor: {
+		hue: number;
+		fixed: boolean;
+	};
+	banner: {
+		enable: boolean;
+		src: string;
+		position?: string;
+	};
 
-  favicon: Favicon[]
-}
+	favicon: Favicon[];
+};
 
 export type Favicon = {
-  src: string
-  theme?: 'light' | 'dark'
-  sizes?: string
-}
+	src: string;
+	theme?: "light" | "dark";
+	sizes?: string;
+};
 
 export enum LinkPreset {
-  Home = 0,
-  Archive = 1,
-  About = 2,
+	Home = 0,
+	Archive = 1,
+	About = 2,
 }
 
 export type NavBarLink = {
-  name: string
-  url: string
-  external?: boolean
-}
+	name: string;
+	url: string;
+	external?: boolean;
+};
 
 export type NavBarConfig = {
-  links: (NavBarLink | LinkPreset)[]
-}
+	links: (NavBarLink | LinkPreset)[];
+};
 
 export type ProfileConfig = {
-  avatar?: string
-  name: string
-  bio?: string
-  links: {
-    name: string
-    url: string
-    icon: string
-  }[]
-}
+	avatar?: string;
+	name: string;
+	bio?: string;
+	links: {
+		name: string;
+		url: string;
+		icon: string;
+	}[];
+};
 
 export type LicenseConfig = {
-  enable: boolean
-  name: string
-  url: string
-}
+	enable: boolean;
+	name: string;
+	url: string;
+};
 
-export type LIGHT_DARK_MODE = typeof LIGHT_MODE | typeof DARK_MODE | typeof AUTO_MODE
+export type LIGHT_DARK_MODE =
+	| typeof LIGHT_MODE
+	| typeof DARK_MODE
+	| typeof AUTO_MODE;
